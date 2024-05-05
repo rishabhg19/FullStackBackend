8b8e65c3adc12840fed74b10f002459997283993
Rishabh Ganesh <70454763+rishabhg19@users.noreply.github.com>
2024-05-05 18:37:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
     res.json({message: "API works"});
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 5000');
 });
